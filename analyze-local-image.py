3ebcf145a5c4fdbfa368682f0df2dde4a4888ead
Amin Ghasemazar <aming@ece.ubc.ca>
2020-08-24 16:14:14 -0700

    trafic imges - coal harbur

---
+++ analyze-local-image.py
@@ -0,0 +1,25 @@
+from utils.cv_functions import *
+from utils.input_data import get_inpu_data
+from utils.sys_check import get_env_variables
+
+'''
+Computer Vision API (v3.0)
+https://westcentralus.dev.cognitive.microsoft.com/docs/services/computer-vision-v3-ga/operations/5d986960601faab4bf452005
+'''
+
+endpoint, key = get_env_variables()
+computervision_client = authenticate_the_client(endpoint, key)
+image_streams = get_local_data()
+
+# Apps
+cvc = computervision_client
+
+for i,img in enumerate(image_streams):
+    print(50*'-')
+    print('Image,\t'+ str(i)+ ',\t' + str(img)+',')
+    for f in [describe_image, categorize_image, tag_image, detect_color_image, get_landmark_image, extract_text_image]:
+        f(cvc, img)
+
+        
+
+
